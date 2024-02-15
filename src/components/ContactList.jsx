570e31dc5434c8f2e7e80e69992a9fe1b2713876
ivette-merc <ivettemerc@gmail.com>
2024-02-14 20:35:22 -0500

    table made

---
+++ src/components/ContactList.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function contactList() {
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th colSpan="3">Contact List</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Name</td>
+            <td>Email</td>
+            <td>Phone</td>
+          </tr>
+        </tbody>
+      </table>
+    );
+}
